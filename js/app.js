32e44d27c13cc9c155639c96fe4b299d38f21df7
Zachary Blanton <zbblanton@gmail.com>
2018-01-15 04:14:16 -0500

    Preparing for merge to master.

--- js/app.js
+++ js/app.js
@@ -1,53 +1,156 @@
-class TopMenuComponent extends React.Component {
+class RegisterGuestFieldsComponent extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
   render() {
     return (
-      <nav className="navbar" role="navigation" aria-label="main navigation">
-        <div className="navbar-brand">
-          <a className="navbar-item" href="/">test</a>
-          <h1 className="navbar-item">WebOps</h1>
-          <button className="button navbar-burger">
-            <span></span>
-            <span></span>
-            <span></span>
-          </button>
-        </div>
-        <div className="navbar-menu">
-          <div className="navbar-start">
-            <a className="navbar-item">Dashboard</a>
-            <div className="navbar-item has-dropdown is-hoverable">
-              <a className="navbar-link">Apps</a>
-              <div className="navbar-dropdown">
-                <a className="navbar-item">All Apps</a>
-                <a className="navbar-item">Supported Apps</a>
-                <hr className="navbar-divider" />
-                <a className="navbar-item">Corporate</a>
-                <a className="navbar-item">Power</a>
-                <a className="navbar-item">Healthcare</a>
-                <a className="navbar-item">Aviation</a>
-                <a className="navbar-item">Capital</a>
-              </div>
-            </div>
-            <a className="navbar-item">Test App</a>
-            <a className="navbar-item">Opal</a>
-            <a className="navbar-item">Decommission</a>
+      <div className="guest-info">
+      <div className="field is-horizontal">
+        <div className="field-body">
+          <div className="field">
+            <p className="control has-icons-left">
+              <input required className="input gFirstname" type="text" placeholder="First Name" />
+              <span className="icon is-small is-left">
+                <i className="fa fa-user"></i>
+              </span>
+            </p>
           </div>
-          <div className="navbar-end">
-            <div className="navbar-item has-dropdown is-hoverable">
-              <a className="navbar-link">All</a>
-              <div className="navbar-dropdown is-right">
-                <a className="navbar-item">Corporate</a>
-                <a className="navbar-item">Power</a>
-                <a className="navbar-item">Healthcare</a>
-                <a className="navbar-item">Aviation</a>
-                <a className="navbar-item">Capital</a>
-              </div>
-            </div>
+          <div className="field">
+            <p className="control has-icons-left">
+              <input required className="input gLastname" type="text" placeholder="Last Name" />
+              <span className="icon is-small is-left">
+                <i className="fa fa-user"></i>
+              </span>
+            </p>
+          </div>
+          <div className="field">
+            <p className="control has-icons-left has-icons-right">
+              <input required className="input gEmail" type="email" placeholder="Email" />
+              <span className="icon is-small is-left">
+                <i className="fa fa-envelope"></i>
+              </span>
+            </p>
           </div>
         </div>
-      </nav>
+      </div>
+      <div className="field is-horizontal">
+        <div className="field-body">
+          <div className="field">
+            <p className="control has-icons-left">
+              <input required className="input gAddress" type="text" />
+              <span className="icon is-small is-left">
+                <i className="fa fa-home"></i>
+              </span>
+            </p>
+          </div>
+        </div>
+      </div>
+      <br />
+      </div>
     );
   }
 }
 
-ReactDOM.render(<TopMenuComponent />, document.getElementById('TopMenu'));
-//ReactDOM.render(<DecomPageComponent />, document.getElementById('Content'));
+class RegisterGuestComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {submitted: false, guests: [0]};
+
+    this.handleAddClick = this.handleAddClick.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleAddClick() {
+    this.state.guests.push(this.state.guests.length);
+    this.setState(this.state)
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    var guestsHTML = document.getElementsByClassName("guest-info")
+    var guests = []
+    for(var i = 0; i < guestsHTML.length; i++){
+      var f = guestsHTML[i].getElementsByClassName("gFirstname")[0].value;
+      var l = guestsHTML[i].getElementsByClassName("gLastname")[0].value;
+      var e = guestsHTML[i].getElementsByClassName("gEmail")[0].value;
+      var a = guestsHTML[i].getElementsByClassName("gAddress")[0].value;
+      guests.push({"firstname": f, "lastname": l, "address": e, "email": a});
+    }
+    this.sendData(JSON.stringify({"recaptcha": grecaptcha.getResponse(), "guests": guests}))
+  }
+
+  sendData(g) {
+    fetch('https://api.mrandmrsblanton.com/add', {
+      method: 'post',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: g
+    }).then(res=>res.json())
+      .then(res => {
+        if(res.success){
+          this.state.submitted = true;
+          this.setState(this.state)
+        }
+      });
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        {
+          this.state.submitted ? (
+            <article className="message is-primary">
+              <div className="message-header">
+                <p>RSVP Submitted</p>
+              </div>
+              <div className="message-body">
+                Thank you for the RSVP. In case of changes we will email you.
+              </div>
+            </article>
+          ) : (
+            this.state.guests.map(function (i) {
+              return (
+                <RegisterGuestFieldsComponent key={i} />
+              );
+            })
+          )
+        }
+        {
+          !this.state.submitted &&
+          <div className="field is-grouped has-addons has-addons-right">
+            <div className="g-recaptcha" data-sitekey="6LfpPC4UAAAAAOTs1q3qVcz8K6zmdE99AwPfWFmX"></div>
+          </div>
+        }
+        {
+          !this.state.submitted &&
+          <div className="field is-grouped has-addons has-addons-right">
+            <p className="control">
+              <a className="button is-light" onClick={this.handleAddClick}>
+                Add Guest
+              </a>
+            </p>
+            <p className="control">
+              <input type="submit" value="Submit RSVP" className="button is-primary" />
+            </p>
+          </div>
+        }
+      </form>
+    );
+  }
+}
+
+class FooterComponent extends React.Component {
+  render() {
+    return (
+      <div className="container has-text-centered">
+        <p><strong>Copyright &copy; 2018 </strong> mrandmrsblanton.com</p>
+        <p>Source at <strong><a href="https://github.com/zbblanton/mrandmrsblanton_main_website">GitHub</a></strong></p>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<RegisterGuestComponent />, document.getElementById('RegisterGuestComponent'));
+ReactDOM.render(<FooterComponent />, document.getElementById('FooterComponent'));
